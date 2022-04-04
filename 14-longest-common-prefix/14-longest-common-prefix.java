5de341bbd4aa4b1ca2da7953ed1180be7ae766c0
akshuanu007 <34731728+akshuanu007@users.noreply.github.com>
2022-04-04 17:12:48 -0400

    Time: 1 ms (81.80%), Space: 42.4 MB (36.10%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,27 +1,26 @@
 class Solution {
      public String longestCommonPrefix(String[] strs) {
-    StringBuilder longestCommonPrefix = new StringBuilder();
-        // Base condition
-        if (strs == null || strs.length == 0) {
-            return longestCommonPrefix.toString();
-        }
-        // Find the minimum length string from the array
-        int minimumLength = strs[0].length();
-        for (int i = 1; i < strs.length; i++) {
-            minimumLength = Math.min(minimumLength, strs[i].length());
-        }
-        // Loop for the minimum length
-        for (int i = 0; i < minimumLength; i++) {
-            // Get the current character from first string
-            char current = strs[0].charAt(i);
-            // Check if this character is found in all other strings or not
-            for (String str : strs) {
-                if (str.charAt(i) != current) {
-                    return longestCommonPrefix.toString();
-                }
-            }
-            longestCommonPrefix.append(current);
-        }
-        return longestCommonPrefix.toString();
+         StringBuilder lcp = new StringBuilder();
+         if(strs[0] == null || strs.length == 0){
+             return lcp.toString();
+         }
+         
+         // find the shortest string //
+         int smallestStringlength = strs[0].length();
+         for(int i =0; i<strs.length; i++){
+             smallestStringlength = Math.min(smallestStringlength, strs[i].length());
+         }
+         
+         
+         for(int i=0; i<smallestStringlength; i++){
+             char currChar = strs[0].charAt(i);
+             for (String str: strs){
+                 if (str.charAt(i) != currChar){
+                     return lcp.toString();
+                 }
+             }
+             lcp.append(currChar);
+         }
+         return lcp.toString();
  }
 }
